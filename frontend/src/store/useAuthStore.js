42ff84fa1f86f0ba4da9f44ede6fbba7fd773877
sanskargupta <sanskarg348@gmail.com>
2025-03-27 22:36:13 +0530

    just commit

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -48,7 +48,7 @@ export const useAuthStore = create((set, get) => ({
       const res = await axiosInstance.post("/auth/google", data);
       set({ authUser: res.data });
       toast.success("Logged in successfully");
-      console.log(res);
+      console.log("Apne", data, res);
       get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
