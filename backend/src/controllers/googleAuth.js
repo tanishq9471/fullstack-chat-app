7c2e678cbdb7fb17824455b70edb0ddf741cc84c
Tanishq Gupta <tanishq9471@gmail.com>
2025-03-28 14:23:10 +0530

    .js

--- backend/src/controllers/googleAuth.js
+++ backend/src/controllers/googleAuth.js
@@ -1,5 +1,5 @@
 import passport from 'passport';
-import {signup} from './auth.controller'
+import {signup} from './auth.controller.js'
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import dotenv from 'dotenv';
 
